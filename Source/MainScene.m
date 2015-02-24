5fc45330ac82e347f6a277581f80a06cab1e5a90
Ian Fischer <ian@apportable.com>
2015-02-24 14:39:24 -0800

    Initial commit

---
+++ Source/MainScene.m
@@ -0,0 +1,13 @@
+#import "MainScene.h"
+
+@implementation MainScene
+
+- (void)showAToast:(id)target {
+    NSLog(@"showAToast");
+}
+
+- (void)showAnotherToast:(id)target {
+    NSLog(@"showAnotherToast");
+}
+
+@end
